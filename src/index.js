4d19b2ebc4f8363fa200df6cf7c2b3d1899ee016
yidahc <yidah.a.curiel@gmail.com>
2019-03-28 00:34:46 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(
+   <App />,
+   document.querySelector('#root')
+);
+
+//https://semantic-ui.com
+//semantic-ui cdn
